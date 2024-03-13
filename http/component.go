a6f5de17ae352a6fff7d09600ee179e711eefb05
lnashier <17622088+lnashier@users.noreply.github.com>
2024-03-13 09:12:46 -0700

    +refactor http client

--- http/component.go
+++ http/component.go
@@ -1,7 +1,7 @@
 package http
 
 // Component defines log-running services within the http service
-// that require graceful shutdown when service shuts down
+// that require graceful shutdown when service shuts down.
 type Component interface {
 	Stop()
 }
